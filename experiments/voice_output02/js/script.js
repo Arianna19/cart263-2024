d6051e75c9b163569d84c8d5d4718af1a718d2fc
Arianna <70153178+Arianna19@users.noreply.github.com>
2024-01-31 20:05:21 -0500

    New experiments

--- experiments/voice_output02/js/script.js
+++ experiments/voice_output02/js/script.js
@@ -1,12 +1,14 @@
 /**
-Title of Project
-Author Name
+Trying the output exercise
+Arianna Narita
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
 "use strict";
+
+const speechSynth = new p5.Speech(); //not planning on changing so name it a constant
 
 
 /**
@@ -21,6 +23,16 @@ function preload() {
 Description of setup
 */
 function setup() {
+    createCanvas(500, 500);
+
+    //thing to make it actually say something
+    console.log(speechSynth.listVoices());
+
+    //synth settings    
+    speechSynth.setPitch(1.4);
+    speechSynth.setRate(2);
+    speechSynth.setVoice(`Microsoft Haruka - Japanese (Japan)`);
+
 
 }
 
@@ -29,5 +41,10 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(201,67,9);
 
+}
+
+function mousePressed() {
+    speechSynth.speak("2024-01-31");
 }
