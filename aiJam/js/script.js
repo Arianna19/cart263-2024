f3bb7ec24fec2335c62d75498db2986338ace2d6
Arianna <70153178+Arianna19@users.noreply.github.com>
2024-02-28 22:21:51 -0500

    Just creating an outline and importing things to the project

--- aiJam/js/script.js
+++ aiJam/js/script.js
@@ -1,17 +1,18 @@
 /**
-Title of Project
-Author Name
+Ai Jam: Draw for Me in Thin Air
+Arianna Narita
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Making my first AI project using ml5.js
+The idea is to make the user draw an image and make the computer guess it 
+See if the computer is able to accurately guess whats happening while user is drawing something
+1. User will have to use their camera
+2. Use the ml5.js classifying and drawing components 
 */
 
 "use strict";
 
 
-/**
-Description of preload
-*/
+
 function preload() {
 
 }
